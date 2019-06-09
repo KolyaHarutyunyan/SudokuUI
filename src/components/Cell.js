8de11d95e20d29e56557f4ff118ff6c2f1eba7cd
KolyaHarutyunyan <kolyah2809@gmail.com>
2019-06-08 21:22:55 -0400

    Run prettier

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,45 +1,57 @@
-import React from 'react';
-import clsx from "clsx";
-import './Cell.css';
+import React from 'react'
+import clsx from 'clsx'
+import './Cell.css'
 
 class Cell extends React.PureComponent {
     constructor(props) {
-        super(props);
-        this.handleMouseHover = this.handleMouseHover.bind(this);
+        super(props)
+        this.handleMouseHover = this.handleMouseHover.bind(this)
 
         this.state = {
             hover: {
                 isHovering: false,
-                value: null
+                value: null,
             },
             pencilMarks: [],
             shouldShowPencilMarks: true, // probably should be a prop?
-            value: null // the "big-number" value (vs. pencil marks)
-        };
+            value: null, // the "big-number" value (vs. pencil marks)
+        }
     }
 
     handleMouseHover(number, isHovering) {
         this.setState({
             hover: {
                 isHovering,
-                value: number
-            }
+                value: number,
+            },
         })
     }
 
     render() {
-        const hoverNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        const hoverNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         return (
             <div className="cell">
-            {
-                hoverNumbers.map(number => {
-                    const hoverAreaClasses = clsx("hoverArea", {
-                        lightPencilMark: this.state.hover.isHovering && this.state.hover.value === number, 
-                        darkPencilMark: this.state.pencilMarks.includes(number)
-                    });
-                    return <div className={hoverAreaClasses} onMouseEnter={() => this.handleMouseHover(number, true)} onMouseLeave={() => this.handleMouseHover(number, false)}>{number}</div>
-                })
-            }
+                {hoverNumbers.map(number => {
+                    const hoverAreaClasses = clsx('hoverArea', {
+                        lightPencilMark:
+                            this.state.hover.isHovering &&
+                            this.state.hover.value === number,
+                        darkPencilMark: this.state.pencilMarks.includes(number),
+                    })
+                    return (
+                        <div
+                            className={hoverAreaClasses}
+                            onMouseEnter={() =>
+                                this.handleMouseHover(number, true)
+                            }
+                            onMouseLeave={() =>
+                                this.handleMouseHover(number, false)
+                            }
+                        >
+                            {number}
+                        </div>
+                    )
+                })}
             </div>
         )
     }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Grid.css'
-import Block from "./Block"
+import Block from './Block'
 
-// TODO: Overflow issues when grid's size is reduced. 
+// TODO: Overflow issues when grid's size is reduced.
 function Grid() {
     return (
         <div className="grid">
