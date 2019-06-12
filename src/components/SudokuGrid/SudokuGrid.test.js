da148b3b9546ec542371812f4a6ba6eeceacabf0
KolyaHarutyunyan <kolyah2809@gmail.com>
2019-06-11 20:11:02 -0400

    Add test for rendering pre-defined values in cells

--- src/components/SudokuGrid/SudokuGrid.test.js
+++ src/components/SudokuGrid/SudokuGrid.test.js
@@ -14,4 +14,16 @@ describe("SudokuGrid", () => {
     it("renders without crashing", () => {
         expect(render(<SudokuGrid {...getProps()} />)).toBeDefined();
     });
+
+    it("renders pre-defined values in cells if provided", () => {
+        const props = getProps({ contents: ["1", "5", "9", "2", "", "", ""] });
+        console.log(props);
+
+        const { getByText, queryByText } = render(<SudokuGrid {...props} />);
+
+        // We must specify the css selector because right now we're secretly rendering
+        // all the numbers in each cell (invisible to the user) to implement hovering logic.
+        expect(getByText("1", { selector: ".valueWrapper" })).toBeDefined();
+        expect(queryByText("3", { selector: ".valueWrapper" })).toBeNull();
+    });
 });
