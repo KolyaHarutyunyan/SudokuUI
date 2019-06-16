ea3c508b717b30502495b907287889e87672022e
KolyaHarutyunyan <kolyah2809@gmail.com>
2019-06-15 21:35:19 -0400

    Styling updates

--- src/components/EntryMethodButtonGroup/EntryMethodButtonGroup.js
+++ src/components/EntryMethodButtonGroup/EntryMethodButtonGroup.js
@@ -8,17 +8,24 @@ export function EntryMethodButtonGroup({ isUsingPencilMarks, toggleEntryMethod }
     const bigButtonClasses = clsx("entryMethodButton", { selected: !isUsingPencilMarks });
     const littleButtonClasses = clsx("entryMethodButton", { selected: isUsingPencilMarks });
     return (
-        <div className="buttonGroup">
-            <button className={bigButtonClasses} type="button" onClick={() => toggleEntryMethod()}>
-                Big
-            </button>
-            <button
-                className={littleButtonClasses}
-                type="button"
-                onClick={() => toggleEntryMethod()}
-            >
-                Little
-            </button>
+        <div className="buttonGroupWrapper">
+            <label>Entry Method</label>
+            <div className="buttonGroup">
+                <button
+                    className={bigButtonClasses}
+                    type="button"
+                    onClick={() => toggleEntryMethod()}
+                >
+                    Big
+                </button>
+                <button
+                    className={littleButtonClasses}
+                    type="button"
+                    onClick={() => toggleEntryMethod()}
+                >
+                    Little
+                </button>
+            </div>
         </div>
     );
 }
